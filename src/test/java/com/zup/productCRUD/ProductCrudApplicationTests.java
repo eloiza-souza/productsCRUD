860a90c1b56087b676715cd17a893acac1ee778f
Eloiza <eloiza.souza@zup.com.br>
2025-01-22 15:07:06 -0300

    feat: create Spring project

---
+++ src/test/java/com/zup/productCRUD/ProductCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.productCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
