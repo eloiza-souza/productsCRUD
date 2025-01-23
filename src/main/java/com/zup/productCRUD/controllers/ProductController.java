1241a176750c98cbff9ec0ada0f967de3e4cbce5
Eloiza <eloiza.souza@zup.com.br>
2025-01-23 10:51:11 -0300

    feat: add class ProductController

--- src/main/java/com/zup/productCRUD/controllers/ProductController.java
+++ src/main/java/com/zup/productCRUD/controllers/ProductController.java
@@ -2,17 +2,21 @@ package com.zup.productCRUD.controllers;
 
 import com.zup.productCRUD.dtos.ProductRequest;
 import com.zup.productCRUD.dtos.ProductResponse;
-import com.zup.productCRUD.mappers.ProductMapper;
-import com.zup.productCRUD.models.Product;
 import com.zup.productCRUD.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -26,9 +30,33 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest productRequest){
-        Product product = new ProductMapper().toEntity(productRequest);
-        productService.save(product);
-        ProductResponse productResponse = new ProductMapper().toResponse(product);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(productRequest));
     }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ProductResponse>> getProductsByName(@RequestParam String name){
+        return ResponseEntity.ok(productService.getProductsByName(name));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductRequest productRequest){
+        return ResponseEntity.ok(productService.updateProduct(id, productRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProductById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
