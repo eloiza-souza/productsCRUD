efa5d01bea0fc02bc5a3496d50e6889f187f66d8
Eloiza <eloiza.souza@zup.com.br>
2025-01-23 11:38:48 -0300

    fix: add getters and setters to ProductResponse

--- src/main/java/com/zup/productCRUD/dtos/ProductResponse.java
+++ src/main/java/com/zup/productCRUD/dtos/ProductResponse.java
@@ -24,4 +24,44 @@ public class ProductResponse {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
